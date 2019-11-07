1f2a4a2106d511eeeac9f0218f10e82279080437
TamaraShy <alisashylko@gmail.com>
2019-11-07 16:48:24 +0200

    “{commit_message}”

--- src/main/java/com/hillel/lecture_6/SquareWordPatternChecker.java
+++ src/main/java/com/hillel/lecture_6/SquareWordPatternChecker.java
@@ -7,15 +7,17 @@ import io.qameta.allure.Step;
  */
 public class SquareWordPatternChecker {
 
-
     @Step
+
     public String applySquareWordPattern(String word) {
 
 //        TODO implements result
-        String result = "";
+        String result = word;
+        String words1 = word;
 
-        return result;
+        for (int i = 1; i < word.length(); i++) {
+            words1 = words1.substring(1) + words1.substring(0, 1);
+            result = result + ", " + words1;
+        }
     }
-
-
 }

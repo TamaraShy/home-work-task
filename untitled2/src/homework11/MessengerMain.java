c17ebfccfed4d7f5703b844d2b375025e19d2ea9
TamaraShy <alisashylko@gmail.com>
2019-11-28 10:25:15 +0200

    add

--- untitled2/src/homework11/MessengerMain.java
+++ untitled2/src/homework11/MessengerMain.java
@@ -6,9 +6,12 @@ public class MessengerMain {
         Telegram Telegram = new Telegram();
         Viber Viber = new Viber();
         WatsApp WatsApp = new WatsApp();
-        User testUser = new User("Pete", Telegram);
+        User testUser = new User("Peter", Telegram, Viber, WatsApp);
 
         testUser.writeSomeText("User writing some text...");
+        testUser.editSomeText("User editing message...");
+        testUser.quoteSomeText("User quote message...");
+        testUser.forwardSomeText("User forward message...");
         testUser.send();
     }
 }
